8c225ff94ce8421561b110ceb0577ec411ff0f40
lgcollins <72031281+lgcollins@users.noreply.github.com>
2021-07-02 11:56:45 -0500

    Update Nets.py

--- models/Nets.py
+++ models/Nets.py
@@ -1,3 +1,6 @@
+# Modified from: https://github.com/pliang279/LG-FedAvg/blob/master/models/Nets.py
+# credit goes to: Paul Pu Liang
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Python version: 3.6
